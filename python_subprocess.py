f234918d408f103505023b0d5b4da21c29905f9d
nattapon44 <nattapon.su@kkumail.com>
2023-12-25 15:33:08 +0700

    test run firstpy.py

--- python_subprocess.py
+++ python_subprocess.py
@@ -1,7 +1,6 @@
-from argparse import _SubParsersAction
-import subprocess #สำำหรับรัน terminal command
+import subprocess #สำหรับรัน terminal command
 
 if __name__ == "__main__":
     #basic terminal command
     subprocess.run(["ls","-ltr"])
-    subprocess.run(["rm","-r","/home/nattapon/testfolder1"])
+    subprocess.run(["python","firstpy.py"])
