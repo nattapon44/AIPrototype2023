9752fca5d9ce3f4b936b163f69d211108e7cfa9c
nattapon44 <nattapon.su@kkumail.com>
2023-12-25 15:09:26 +0700

    test process

--- python_subprocess.py
+++ python_subprocess.py
@@ -1,5 +1,8 @@
+from argparse import _SubParsersAction
 import subprocess #สำำหรับรัน terminal command
 
 if __name__ == "__main__":
     #basic terminal command
     subprocess.run(["ls","-ltr"])
+    subprocess.run(["rm","-r","~/test"])
+    subprocess.run(["cd"])
