4a0c08347da8862ae4fad6ca1f08ae288027079b
nattapon44 <nattapon.su@kkumail.com>
2023-12-25 15:38:54 +0700

    test run firstpy.py

--- python_subprocess.py
+++ python_subprocess.py
@@ -2,5 +2,6 @@ import subprocess #สำหรับรัน terminal command
 
 if __name__ == "__main__":
     #basic terminal command
-    subprocess.run(["ls","-ltr"])
-    subprocess.run(["python","firstpy.py"])
+    subprocess.run(["python","firstpy.py","--num","100","--XX","90"])
+    subprocess.run(["python","firstpy.py","--num","-10","--XX","-90"])
+    subprocess.run(["python","firstpy.py","--num","0"])
