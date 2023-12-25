52a7e72b0831136dced5f0a6509e97b2a30cdb8a
nattapon44 <nattapon.su@kkumail.com>
2023-12-25 15:50:20 +0700

    test run firstpy.py

--- python_subprocess.py
+++ python_subprocess.py
@@ -1,7 +1,22 @@
 import subprocess #สำหรับรัน terminal command
 
+def firstrun():
+    print("first run num=100 XX=90")
+def secrun():
+    print("first run num=-10 XX=-90")
+def thirdrun():
+    print("first run num=0")
+def print():
+    print("---------------------------------------")
+    
 if __name__ == "__main__":
     #basic terminal command
+    firstrun()
     subprocess.run(["python","firstpy.py","--num","100","--XX","90"])
+    print()
+    secrun()
     subprocess.run(["python","firstpy.py","--num","-10","--XX","-90"])
+    print()
+    thirdrun()
     subprocess.run(["python","firstpy.py","--num","0"])
+    print()
