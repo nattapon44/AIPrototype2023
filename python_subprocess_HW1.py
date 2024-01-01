e127b9003aa7e3d34cad434133cb6950e4746dcd
nattapon44 <nattapon.su@kkumail.com>
2024-01-02 04:17:34 +0700

    add Popen

--- python_subprocess_HW1.py
+++ python_subprocess_HW1.py
@@ -20,3 +20,21 @@ print(out.decode('utf-8'))
 print(len(out.decode('utf-8')))
 
 #Hw เขียน subprocess sum output ที่งหมดของ 3 อันข้างบน (ตัวเลขก่อน Hello World!)
+
+total_sum = 0
+
+output1 = subprocess.check_output(["python", "firstpy.py", "--num", "100", "--XX", "90"]).decode('utf-8')
+sum_value1 = int(output1.split('\n')[3])  # ดึงค่าที่อยู่บรรทัดที่ 4
+total_sum += sum_value1
+
+output2 = subprocess.check_output(["python", "firstpy.py", "--num", "-10", "--XX", "-90"]).decode('utf-8')
+sum_value2 = int(output2.split('\n')[3])  # ดึงค่าที่อยู่บรรทัดที่ 4
+total_sum += sum_value2
+
+output3 = subprocess.check_output(["python", "firstpy.py", "--num", "0"]).decode('utf-8')
+sum_value3 = int(output3.split('\n')[3])  # ดึงค่าที่อยู่บรรทัดที่ 4
+total_sum += sum_value3
+
+print("Subprocess sum output")
+print(total_sum)
+
