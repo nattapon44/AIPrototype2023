41b3c4f279e539b502115753a7e275962e873c62
nattapon44 <nattapon.su@kkumail.com>
2024-01-02 03:16:11 +0700

    add subprocess sum output

--- python_subprocess.py
+++ python_subprocess.py
@@ -19,4 +19,15 @@ out, err = process_output.communicate()
 print(out.decode('utf-8'))
 print(len(out.decode('utf-8')))
 
+process_output1 = subprocess.Popen(["python","firstpy.py","--num","100","--XX","90"],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+out, err = process_output1.communicate()
+process_output2 = subprocess.Popen(["python","firstpy.py","--num","-10","--XX","-90"],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+out, err = process_output2.communicate()
+process_output3 = subprocess.Popen(["python","firstpy.py","--num","0"],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+out, err = process_output3.communicate()
+sum_output = process_output1 + process_output2 + process_output3
+print("Subprocess sum output")
+print(sum_output)
+
 #Hw เขียน subprocess sum output ที่งหมดของ 3 อันข้างบน (ตัวเลขก่อน Hello World!)
+
