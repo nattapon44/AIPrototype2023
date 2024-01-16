6efd8b5ce6b4f6513c81589c6516a15dbff3812a
nattapon44 <nattapon.su@kkumail.com>
2024-01-16 11:12:27 +0700

    added upload file

--- firstflask.py
+++ firstflask.py
@@ -26,5 +26,22 @@ def homefn():
         print(lastnamein, file=sys.stdout)
         return render_template("home.html",name=namein)
 
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        file.save('filename')
+        return render_template("home.html",name='upload completed')
+
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True,port=5001)
