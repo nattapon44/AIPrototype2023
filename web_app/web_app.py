6115acdd3b095b8b2596bebc2853af3f5d8c0c2c
nattapon44 <nattapon.su@kkumail.com>
2024-02-27 01:26:25 +0700

    web_app

---
+++ web_app/web_app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, render_template, make_response 
+import json
+import sys
+
+app = Flask(__name__)
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
+    
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        file = request.files['file']
+        file.save('filename')
+        return render_template("home.html",name='upload completed')
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',debug=True,port=5001)
